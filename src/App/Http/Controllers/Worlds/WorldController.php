18f9d12e1305f4c9ce588fbb73a416db0d59fb5f
Jim <jacretney@gmail.com>
2022-05-08 18:04:18 +0100

    Logic to create an existing world or a new world

---
+++ src/App/Http/Controllers/Worlds/WorldController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Substratum\App\Http\Controllers\WorldController;
+
+use Illuminate\Http\Request;
+use Substratum\App\Http\Controllers\Controller;
+use Substratum\App\Http\Requests\CreateWorldRequest;
+use Substratum\Domain\Worlds\CreateOptionEnum;
+use Substratum\Domain\Worlds\World;
+use Substratum\Domain\Worlds\WorldService;
+
+class WorldController extends Controller
+{
+    public function __construct(
+        private WorldService $worldService
+    ){}
+
+    public function create(CreateWorldRequest $request)
+    {
+        $createOption = CreateOptionEnum::fromValue();
+
+        $path = env('WORLD_PATH');
+
+        if (! $path) {
+            return redirect()->back()->with('error', 'Please ensure a WORLD_PATH has been specified in your .env file');
+        }
+
+        $this->worldService->create($createOption, [
+            'name' => $request->input('name'),
+            'path' => $path . '/' . str()->snake($request->input('name')),
+            'file' => $request->file('file')->getRealPath(),
+        ]);
+    }
+}
